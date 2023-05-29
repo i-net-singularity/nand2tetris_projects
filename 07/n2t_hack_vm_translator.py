48f315f9a7a195b4987898fe4fb4116c66ce1dc7
natosepia <natosenet@gmail.com>
2023-05-30 03:02:50 +0900

    算術演算、メモリアクセス　7章完成

--- 07/n2t_hack_vm_translator.py
+++ 07/n2t_hack_vm_translator.py
@@ -3,6 +3,8 @@ import sys,os
 from typing import List,Dict
 
 VM_COMENT = '//'
+VM_TRUE   = '-1'
+VM_FALSE  = '0'
 
 class N2THackVMTranslator(object):
 
@@ -16,15 +18,10 @@ class N2THackVMTranslator(object):
         self.parsed_vm_codes = {}       # ファイルごとにVMコードのパース結果を格納する
         self.asm_codes = []
 
-        #self.symbol_table_manager = HackSymbolTableManager()
-
     def run(self):
         self.prepare_file_paths(target_file_path)                               # 入力ファイル名と出力ファイル名を設定
         self.load_vm_codes()                                                    # 入力ファイルの内容をリストに取り込む
         self.vm_tranlate()
-        #self.convert_asm_codes()
-        #self.write_machine_codes()
-        pass
 
     def prepare_file_paths(self, file_path):
         print(" file_path = " + file_path)
@@ -76,18 +73,14 @@ class N2THackVMTranslator(object):
 
         code_writer = CodeWriter(self.asm_file)
 
-        for filepath, lines in self.vm_codes.items():
-            print(f' File: {filepath}')
+        for vm_filepath, lines in self.vm_codes.items():
+            print(f' File: {vm_filepath}')
             # 1行ずつVMコードを変換
             vm_parser = VMParser()
             vm_parser.vm_parse(lines)
             #vm_parser.increment()
 
-            code_writer.write_assembly(vm_parser.parsed_vm_codes)
-
-            #for line in vm_parser.parsed_vm_codes:
-            #    print(f'{vm_parser.count,line}')
-            print('---')  # 各ファイルが終わったことを示すための区切り線
+            code_writer.write_assembly(vm_filepath,vm_parser.parsed_vm_codes)
 
         code_writer.close()
 
@@ -161,15 +154,18 @@ class CodeWriter(object):
             'argument' : 'ARG',
             'this'     : 'THIS',
             'that'     : 'THAT',
-            'pointer'  : 'R3',
-            'temp'     : 'R5',
+            'pointer'  : '3',
+            'temp'     : '5',
             'static'   : 'R16',
+            'constant' : 'undefined'    # 実メモリへの割り当てなし
         }
 
         # ラベルのカウント：条件分岐毎にインクリメントし固有のラベルを作成する
         self.label_count = 1
 
-    def write_assembly(self, parsed_vm_codes):
+    def write_assembly(self, vm_filepath,parsed_vm_codes):
+        # VMファイル名を設定
+        self.vm_filename=vm_filepath.split('/')[-1].split('.')[0]
         # コマンドタイプに応じてアセンブリコードを出力
         for command in parsed_vm_codes:
             vm_code      = str(command['vm_code'])
@@ -177,102 +173,128 @@ class CodeWriter(object):
             arg1         = str(command['arg1'])
             arg2         = str(command['arg2'])
 
-            self.output_line("// " + vm_code)    # アセンブリの元となったVMコードを出力
+            self.output_line(f"{VM_COMENT} {vm_code}")    # アセンブリの元となったVMコードを出力
 
             if   command_type == "C_ARITHMETIC" :
-                self.write_arithmetic(arg1,arg2)
+                self.write_arithmetic(arg1)
 
-            elif command_type == "C_PUSH":
-                self.write_push_pop("C_PUSH",arg1,arg2)
-            elif command_type == "C_POP":
-                self.write_push_pop("C_POP" ,arg1,arg2)
+            elif command_type in ("C_PUSH","C_POP"):
+                self.write_push_pop(command_type,arg1,arg2)
             else:
                 pass
         
-    def write_arithmetic(self,arg1,arg2):
+    def write_arithmetic(self,command):
 
         self.pop_stack_to_D()
 
         # unary operator
-        if arg1 in ("neg","not"):
+        if command == "neg":
+            self.output_line("M=-D") 
 
-            if arg1 == "neg":
-                self.output_line("M=-D") 
-
-            if arg1 == "not":
-                self.output_line("M=!D") 
+        elif command == "not":
+            self.output_line("M=!D") 
 
         # binary operator
-        elif arg1 in ("add","sub","and","or"):
+        elif command in ("add","sub","and","or"):
             self.sp_decrement()
             self.output_line("A=M")
-            if   arg1 == "add":
+            if   command == "add":
                 self.output_line("M=D+M") 
-            elif arg1 == "sub":
+            elif command == "sub":
                 self.output_line("M=M-D")
-            elif arg1 == "and":
+            elif command == "and":
                 self.output_line("M=D&M")
-            elif arg1 == "or":
+            elif command == "or":
                 self.output_line("M=D|M")
 
-        elif arg1 in ("eq","gt","lt"):
+        # Comparison operator
+        elif command in ("eq","gt","lt"):
             self.sp_decrement()
             self.output_line("A=M")
             self.output_line("D=M-D")
 
-            # Comparison part
-            label_true  = "LABEL" + str(self.label_count).zfill(5) + "_TRUE"
-            label_end   = "LABEL" + str(self.label_count).zfill(5) + "_END"
+            label_true = f"LABEL{str(self.label_count).zfill(5)}_TRUE"
+            label_end  = f"LABEL{str(self.label_count).zfill(5)}_END"
 
             self.output_line("@" + label_true)
-            if arg1 == "eq":
+            if command == "eq":
                 self.output_line("D;JEQ")
-            elif arg1 == "gt":
+            elif command == "gt":
                 self.output_line("D;JGT")
-            elif arg1 == "lt":
+            elif command == "lt":
                 self.output_line("D;JLT")
 
             # if false
-            self.output_line("@SP")
-            self.output_line("A=M")
-            self.output_line("M=0") # set false value
-            self.output_line("@" + label_end)
+            self.set_A_to_stack()
+            self.output_line(f"M={VM_FALSE}") # set false valueW
+            self.output_line(f"@{label_end}")
             self.output_line("0;JMP")
             
             # if true
-            self.output_line("(" + label_true + ")")
-            self.output_line("@SP")
-            self.output_line("A=M")
-            self.output_line("M=-1") #set true value (-1 = 0xffff)
-
-            self.output_line("(" + label_end + ")")
+            self.output_line(f"({label_true})")
+            self.set_A_to_stack()
+            self.output_line(f"M={VM_TRUE}") #set true value (-1 = 0xffff)
+            self.output_line(f"({label_end})")
             self.label_count += 1
 
         self.sp_increment()
 
-    def write_push_pop(self,command_type,arg1,arg2):
-        # memo address 解決必要あり
-        if(command_type == "C_PUSH"):
-            if(arg1 == "constant"):
-                self.output_line("@" + arg2)
-                self.output_line("D=A")
-                self.push_D_to_stack()
+    def write_push_pop(self,command_type,segment,index):
+        '''
+        command_type: C_PUSH or C_POP
+        segment: メモリセグメント
+        index  : インデックス
+        '''
 
-        if(command_type == "C_POP"):
-            self.output_line("@" + arg2)
+        # (1) セグメントに対応したメモリアドレスを取得する
+        segment_address = self.segment_dict[segment]
+
+        # (2) セグメントとインデックスからPUSH/POP位置を計算する
+        if(segment == "constant"):
+            self.output_line(f"@{index}")
+        else:
+            if(segment in ("local","argument","this","that")):
+                self.output_line(f"@{segment_address}")
+                self.output_line("D=M")
+                self.output_line(f"@{index}")
+                self.output_line("A=D+A")
+
+            elif(segment in ("pointer","temp")):
+                self.output_line(f"@R{str(int(segment_address) + int(index))}")
+
+            elif(segment == "static"):
+                self.output_line(f"@{self.vm_filename}.{index}")
+ 
+        # (3) PUSH/POPを実行する
+        if(command_type == "C_PUSH"):
+            if(segment == "constant"):
+                 self.output_line("D=A")    # 定数(constant)の場合はAレジスタに値をセット
+            else:
+                 self.output_line("D=M")    # 定数以外はメモリから値を取得　(メモリアドレスは(2)で計算済み)
+            self.push_D_to_stack()
+
+        elif(command_type == "C_POP"):
+             self.output_line("D=A")
+             self.output_line("@R13")
+             self.output_line("M=D")     # R13にPOP先のアドレスを保存
+             self.pop_stack_to_D()
+             self.output_line("@R13")
+             self.output_line("A=M")
+             self.output_line("M=D")
 
     def push_D_to_stack(self):
-        '''Push from D onto top of stack, increment @SP'''
-        self.output_line("@SP") # Get current stack pointer
-        self.output_line("A=M") # Set address to current stack pointer
+        self.set_A_to_stack()
         self.output_line("M=D") # Write data to top of stack
         self.sp_increment()     # Increment SP
 
     def pop_stack_to_D(self):
-        '''Decrement @SP, pop from top of stack onto D'''
         self.sp_decrement()
         self.output_line("A=M")
         self.output_line("D=M")
+
+    def set_A_to_stack(self):
+        self.output_line("@SP") # Get current stack pointer
+        self.output_line("A=M") # Set address to current stack pointer
 
     def sp_increment(self):
         self.output_line("@SP")
@@ -281,21 +303,6 @@ class CodeWriter(object):
     def sp_decrement(self):
         self.output_line("@SP")
         self.output_line("M=M-1")
-
-    #def push_D_to_stack(self):
-    #    '''Push from D onto top of stack, increment @SP'''
-    #    self.write('@SP') # Get current stack pointer
-    #    self.write('A=M') # Set address to current stack pointer
-    #    self.write('M=D') # Write data to top of stack
-    #    self.write('@SP') # Increment SP
-    #    self.write('M=M+1')
-
-    #def pop_stack_to_D(self):
-    #    '''Decrement @SP, pop from top of stack onto D'''
-    #    self.write('@SP')
-    #    self.write('M=M-1') # Decrement SP
-    #    self.write('A=M') # Set address to current stack pointer
-    #    self.write('D=M') # Get data from top of stack
 
     def output_line(self,mnemonic):
         print(mnemonic,file=self.output_file)
